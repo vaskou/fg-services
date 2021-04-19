3ab59fc79e7cfd9926a2d13482b80f8dff8e8866
Vasilis Koutsopoulos <vaskou@hotmail.com>
2021-04-19 19:55:59 +0300

    -Init

---
+++ fg-services.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       FremeditiGuitars - Services
+ * Description:       FremeditiGuitars - Servicess Post Type
+ * Version:           1.0.0
+ * Author:            Vasilis Koutsopoulos
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       fg-services
+ * Domain Path:       /languages
+ */
+
+defined( 'ABSPATH' ) or die();
+
+define( 'FG_SERVICES_VERSION', '1.0.0' );
+define( 'FG_SERVICES_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FG_SERVICES_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'FG_SERVICES_PLUGIN_DIR_NAME', basename( FG_SERVICES_PLUGIN_DIR_PATH ) );
+define( 'FG_SERVICES_PLUGIN_URL', plugins_url( FG_SERVICES_PLUGIN_DIR_NAME ) );
+
+
