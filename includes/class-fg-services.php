74ed2db5dd0f6d3a134910c1129ea4e888de2da2
Vasilis Koutsopoulos <vaskou@hotmail.com>
2021-09-03 19:57:48 +0300

    -WIP

--- includes/class-fg-services.php
+++ includes/class-fg-services.php
@@ -16,6 +16,8 @@ class FG_Services {
 
 	private function __construct() {
 		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
+		add_filter( 'fremediti_guitars_single_content_show_posted_on', array( $this, 'single_content_show_posted_on' ) );
+		add_filter( 'fremediti_guitars_has_sidebar', array( $this, 'has_sidebar' ) );
 
 		FG_Services_Post_Type::instance();
 		FG_Services_Shortcodes::instance();
@@ -23,5 +25,24 @@ class FG_Services {
 
 	public function on_plugins_loaded() {
 		load_plugin_textdomain( 'fg-services', false, FG_SERVICES_PLUGIN_DIR_NAME . '/languages/' );
+	}
+
+	/**
+	 * @param array $post_types
+	 */
+	public function single_content_show_posted_on( $post_types ) {
+
+		$post_types[] = FG_Services_Post_Type::POST_TYPE_NAME;
+
+		return $post_types;
+	}
+
+	public function has_sidebar( $has_sidebar ) {
+
+		if ( is_singular( FG_Services_Post_Type::POST_TYPE_NAME ) ) {
+			$has_sidebar = true;
+		}
+
+		return $has_sidebar;
 	}
 }
